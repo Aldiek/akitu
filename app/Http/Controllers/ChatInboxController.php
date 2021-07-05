c91375033f5a8944beb88d468c7846358c5e2e3a
Mohamed Aldiek <Mohamed.Aldiek@gmail.com>
2021-07-05 08:58:17 -0400

    Update ChatInboxController.php

--- app/Http/Controllers/ChatInboxController.php
+++ app/Http/Controllers/ChatInboxController.php
@@ -26,8 +26,10 @@ class ChatInboxController extends Controller
     public function show($id) {
         $usr=User::find($id);
 
-
-        if (auth()->user()->utype === 'ADM' && auth()->user()->department === $usr->department && $usr->utype === 'USR') {
+          if ($usr->utype == null || $usr->department == null ){
+            abort(404);
+        }
+       else if (auth()->user()->utype === 'ADM' && auth()->user()->department === $usr->department && $usr->utype === 'USR') {
              $sender = User::findOrFail($id);
 
         $users = User::with(['message' => function($query) {
@@ -39,9 +41,6 @@ class ChatInboxController extends Controller
         if (auth()->user()->utype === 'USR') {
             $messages = Message::where('user_id', auth()->id())->orWhere('receiver', auth()->id())->orderBy('id', 'DESC')->get();
         } 
-            else if ($usr->utype == null || $usr->department == null ){
-            abort(404);
-        }
             else {
             $messages = Message::where('user_id', $sender)->orWhere('receiver', $sender)->orderBy('id', 'DESC')->get();
         }
